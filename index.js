1da93f4adb6a3c29a1590cff7cd07328fc32d787
Olena Horovenko <elena.gorovenko@gmail.com>
2024-04-30 23:55:49 +0200

    html added  through js done

--- index.js
+++ index.js
@@ -16,9 +16,22 @@ const product3 = {
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
+const product1Content = `
+  <img class="card-img-top" src=${product1.image} alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+
 const product1El = document.querySelector('#product1');
 
 product1El.classList.add('border-primary');
+
+product1El.innerHTML += product1Content;
 
 const product2Button = document.querySelector('#product2 button');
 
